f922082f7fdcb85d58f1236c52ff41772427a06f
ricardo <ricardo@in8.com.br>
2023-01-31 18:55:30 -0300

    Project created

---
+++ src/main/java/com/estudandojava/workshopspringboot/WorkshopSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.estudandojava.workshopspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopSpringbootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/estudandojava/workshopspringboot/WorkshopSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.estudandojava.workshopspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
