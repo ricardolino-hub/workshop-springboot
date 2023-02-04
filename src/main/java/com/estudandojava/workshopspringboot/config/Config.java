85469916ae11f0e2e6beb1b6bb6985ce372b53b3
ricardo <ricardo@in8.com.br>
2023-02-04 15:46:28 -0300

    Category

--- src/main/java/com/estudandojava/workshopspringboot/config/Config.java
+++ src/main/java/com/estudandojava/workshopspringboot/config/Config.java
@@ -7,9 +7,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 
+import com.estudandojava.workshopspringboot.entities.Category;
 import com.estudandojava.workshopspringboot.entities.Order;
 import com.estudandojava.workshopspringboot.entities.User;
 import com.estudandojava.workshopspringboot.entities.enums.OrderStatus;
+import com.estudandojava.workshopspringboot.repositories.CategoryRepository;
 import com.estudandojava.workshopspringboot.repositories.OrderRepository;
 import com.estudandojava.workshopspringboot.repositories.UserRepository;
 
@@ -20,9 +22,18 @@ public class Config implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2,cat3));
+		
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
 		
--- src/main/java/com/estudandojava/workshopspringboot/services/UserService.java
+++ src/main/java/com/estudandojava/workshopspringboot/services/UserService.java
@@ -11,7 +11,6 @@ import com.estudandojava.workshopspringboot.repositories.UserRepository;
 
 @Service
 public class UserService {
-	
 	@Autowired
 	private UserRepository repository;
 	
