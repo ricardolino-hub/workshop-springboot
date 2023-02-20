ee3f4745a9ef539608f0fbc0e91f39c879a5e3d3
ricardo <ricardo@in8.com.br>
2023-02-20 12:39:05 -0300

    Exception handling - update

--- src/main/java/com/estudandojava/workshopspringboot/services/UserService.java
+++ src/main/java/com/estudandojava/workshopspringboot/services/UserService.java
@@ -13,6 +13,8 @@ import com.estudandojava.workshopspringboot.repositories.UserRepository;
 import com.estudandojava.workshopspringboot.services.exceptions.DatabaseException;
 import com.estudandojava.workshopspringboot.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 	@Autowired
@@ -42,9 +44,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}	
 	}
 
 	private void updateData(User entity, User obj) {
